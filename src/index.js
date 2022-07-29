6afaba58b97a7e2ce2aae8a969d8f421ce354846
eremes-cross <sergei.cross@yandex.by>
2022-07-29 19:20:11 +0300

    reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let str = Math.abs(n).toString();
+    let reversed = ``;
   
-}
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+  
+    return +reversed;
+  }
